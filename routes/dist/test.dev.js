9210eda02a9a4de9b12c8f33f84a6bbe622d81ee
f0lder <40365824+f0lder@users.noreply.github.com>
2020-09-24 21:11:47 +0300

    up

--- routes/dist/test.dev.js
+++ routes/dist/test.dev.js
@@ -6,6 +6,6 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
-  res.render('test');
+  res.render('test'); //return static webite ,no parameters
 });
 module.exports = router;
--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('test');
+    res.render('test'); //return static webite ,no parameters
 });
 
 module.exports = router;
