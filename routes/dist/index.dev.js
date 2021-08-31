93655cf0b29088e3f6eab61a5fe59fc857d88342
f0lder <ursanbogdan01@gmail.com>
2021-08-31 16:46:15 +0300

    alo

--- routes/dist/index.dev.js
+++ routes/dist/index.dev.js
@@ -11,37 +11,71 @@ var assert = require("assert");
 var _require = require('os'),
     type = _require.type;
 
+var _require2 = require('mongodb'),
+    ObjectId = _require2.ObjectId;
+
 var url = "mongodb://localhost:27017/maindb";
 /* GET home page. */
 
-router.get('/', function (req, res, next) {
-  res.render('index');
-});
-router.get('/get-data', function (req, res, next) {
-  var resultArray = [];
+function generatePages(dbname, colname) {
   mongo.connect(url, function (err, db) {
-    var dbo = db.db("maindb");
-    dbo.collection("produsts").find({}).toArray(function (err, result) {
-      if (err) throw err;
-      console.log(result[0]);
-      res.render("index", {
-        "results": result
+    var dbo = db.db(dbname);
+    dbo.collection(colname).find({}).toArray(function (err, result) {
+      if (err) throw err; //  console.log(result);
+
+      result.forEach(function (element) {
+        router.get("/name=" + element.name, function (req, res, next) {
+          res.render("prodid", {
+            "name": element.name,
+            "price": element.price,
+            "title": "ITEM",
+            "desc": element.desc
+          });
+        });
       });
       db.close();
     });
   });
-  console.log(resultArray); // res.render("index", { "results": resultArray });
+}
+
+generatePages("maindb", "products");
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+router.get('/p', function (req, res, next) {
+  mongo.connect(url, function (err, db) {
+    var dbo = db.db("maindb");
+    dbo.collection("products").find({}).toArray(function (err, result) {
+      if (err) throw err;
+      res.render("prod", {
+        "res": result
+      });
+      db.close();
+    });
+  });
+});
+router.get('/get-data', function (req, res, next) {
+  mongo.connect(url, function (err, db) {
+    var dbo = db.db("maindb");
+    dbo.collection("products").find({}).toArray(function (err, result) {
+      if (err) throw err;
+      res.render("index", {
+        "res": result
+      });
+      db.close();
+    });
+  });
 });
 router.post('/insert', function (req, res, next) {
   var item = {
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author
+    name: req.body.name,
+    price: parseInt(req.body.price),
+    desc: req.body.desc
   };
   mongo.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("maindb");
-    dbo.collection("produsts").insertOne(item, function (err, res) {
+    dbo.collection("products").insertOne(item, function (err, res) {
       if (err) throw err;
       console.log("inserted!");
       db.close();
@@ -49,6 +83,44 @@ router.post('/insert', function (req, res, next) {
   });
   res.redirect('/');
 });
-router.post("/update", function (req, res, next) {});
-router.post("/delete", function (req, res, next) {});
+router.post("/update", function (req, res, next) {
+  mongo.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("maindb");
+    var ID = req.body.id;
+    var item = {
+      name: req.body.name,
+      price: req.body.price,
+      desc: req.body.desc
+    };
+    var query = {
+      "_id": ObjectId(ID)
+    };
+    var newvalues = {
+      $set: item
+    };
+    dbo.collection("products").updateOne(query, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      db.close();
+    });
+  });
+  res.redirect("/");
+});
+router.post("/delete", function (req, res, next) {
+  mongo.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("maindb");
+    var ID = req.body.id;
+    var query = {
+      "_id": ObjectId(ID)
+    };
+    dbo.collection("products").deleteOne(query, function (err, res) {
+      if (err) throw err;
+      console.log("1 document deleted");
+      db.close();
+    });
+  });
+  res.redirect("/");
+});
 module.exports = router;
